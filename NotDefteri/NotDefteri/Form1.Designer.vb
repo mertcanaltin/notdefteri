2fa5ea14b70dd797bc056db84b19a70bb76e654d
Mert Can Altın <37827216+mertcanaltin@users.noreply.github.com>
2018-04-11 10:36:06 +0300

    Add files via upload

---
+++ NotDefteri/NotDefteri/Form1.Designer.vb
@@ -0,0 +1,99 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.ButtonSil = New System.Windows.Forms.Button()
+        Me.ButtonNormal = New System.Windows.Forms.Button()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.ButtonNormal)
+        Me.Panel1.Controls.Add(Me.ButtonSil)
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(687, 56)
+        Me.Panel1.TabIndex = 0
+        '
+        'StatusStrip1
+        '
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 351)
+        Me.StatusStrip1.Name = "StatusStrip1"
+        Me.StatusStrip1.Size = New System.Drawing.Size(687, 22)
+        Me.StatusStrip1.TabIndex = 1
+        Me.StatusStrip1.Text = "StatusStrip1"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TextBox1.Location = New System.Drawing.Point(0, 56)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(687, 295)
+        Me.TextBox1.TabIndex = 2
+        '
+        'ButtonSil
+        '
+        Me.ButtonSil.Location = New System.Drawing.Point(528, 3)
+        Me.ButtonSil.Name = "ButtonSil"
+        Me.ButtonSil.Size = New System.Drawing.Size(75, 47)
+        Me.ButtonSil.TabIndex = 0
+        Me.ButtonSil.Text = "Boşluk Sil"
+        Me.ButtonSil.UseVisualStyleBackColor = True
+        '
+        'ButtonNormal
+        '
+        Me.ButtonNormal.Location = New System.Drawing.Point(609, 3)
+        Me.ButtonNormal.Name = "ButtonNormal"
+        Me.ButtonNormal.Size = New System.Drawing.Size(75, 47)
+        Me.ButtonNormal.TabIndex = 1
+        Me.ButtonNormal.Text = "Normal Tümce Düzeni"
+        Me.ButtonNormal.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(687, 373)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.StatusStrip1)
+        Me.Controls.Add(Me.Panel1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.Panel1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents ButtonNormal As System.Windows.Forms.Button
+    Friend WithEvents ButtonSil As System.Windows.Forms.Button
+    Friend WithEvents StatusStrip1 As System.Windows.Forms.StatusStrip
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+
+End Class
---
+++ NotDefteri/NotDefteri/Form1.vb
@@ -0,0 +1,15 @@
+﻿Public Class Form1
+
+    Private Sub ButtonSil_Click(sender As Object, e As EventArgs) Handles ButtonSil.Click
+        Dim Metin As String = TextBox1.Text.Trim
+        Dim YeniMetin As String = Metin(0)
+        For i = 1 To Metin.Length - 1
+            If Metin(i - 1) <> " " Then
+                YeniMetin &= Metin(i)
+            ElseIf Metin(i) <> " " Then
+                YeniMetin &= Metin(i)
+            End If
+        Next
+        TextBox1.Text = YeniMetin
+    End Sub
+End Class
